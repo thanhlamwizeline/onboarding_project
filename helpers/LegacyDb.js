95a820c54efe77d332800151c657e98c0ba3d7d6
Thanh Lam <thanh.lam@wizeline.com>
2025-04-09 13:53:03 +0700

    move config outside the class

--- helpers/LegacyDb.js
+++ helpers/LegacyDb.js
@@ -2,21 +2,21 @@ const { Helper } = require('codeceptjs');
 const sql = require('mssql');
 require('dotenv').config();
 
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  port: parseInt(process.env.DB_PORT, 10),
+  database: process.env.DB_DATABASE,
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+    instanceName: process.env.DB_INSTANCE
+  }
+};
+
 class LegacyDb extends Helper {
   async getJanusPersonRecord() {
-    const config = {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      server: process.env.DB_SERVER,
-      port: parseInt(process.env.DB_PORT, 10),
-      database: process.env.DB_DATABASE,
-      options: {
-        encrypt: false,
-        trustServerCertificate: true,
-        instanceName: process.env.DB_INSTANCE
-      }
-    };
-
     try {
       const pool = await sql.connect(config);
       const result = await pool.request().query(`
